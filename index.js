c789ac09508e7e9cdb459fa4f15707fda530d62e
rhun-akulloh <rhunakulloh@gmail.com>
2026-04-22 14:54:40 +0300

    feat: imliment async await assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+function displayPosts(posts) {
+    const ul = document.getElementById('post-list');
+    for (let post of posts) {
+        const li = document.createElement('li');
+        const h1 = document.createElement('h1');
+        h1.textContent = post.title;
+        const p = document.createElement('p');
+        p.textContent = post.body;
+        li.appendChild(h1);
+        li.appendChild(p);
+        ul.appendChild(li);
+    }
+}
+
+async function fetchPosts() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await response.json();
+    displayPosts(posts);
+}
+
+fetchPosts();
